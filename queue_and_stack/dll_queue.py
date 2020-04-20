38d4a22245089ab9921691521045d0859d893003
Evans Ibok <evansibok@gmail.com>
2020-04-20 16:15:18 +0100

    implement isEmpty queue method

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -3,21 +3,33 @@
 from doubly_linked_list import DoublyLinkedList
 
 
+class NodeList:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.head = node
         # Why is our DLL a good choice to store our elements?
         self.storage = DoublyLinkedList()
 
-    def peek(self):
-        if self.storage.head is None:
-            return "Queue is empty!"
-        else:
-            return self.storage.head
+    def isEmpty(self):
+        return self.storage.head is None
+
+    # def peek(self):
+    #     if self.storage.head is None:
+    #         return self.storage.head
+    #     else:
+    #         return self.storage.head
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        new_node = NodeList(value, None)
+        if self.storage.tail is not None:
+            self.storage.tail = new_node
+        # pass
 
     def dequeue(self):
         pass
@@ -29,4 +41,4 @@ class Queue:
 qb = Queue()
 
 
-print(qb.peek())
+print(qb.isEmpty())

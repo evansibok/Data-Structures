33a7248567fd22a1aa4faff66d92332d73e7af6a
Evans Ibok <evansibok@gmail.com>
2020-04-24 11:08:18 +0100

    create delete method, remove_from_head and remove_from_tail

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -56,6 +56,7 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly."""
 
     def add_to_head(self, value):
+        # increment the length of the list on addition
         self.length += 1
         # instantiate a new node setting prev and next to None
         new_node = ListNode(value, None, None)
@@ -80,22 +81,24 @@ class DoublyLinkedList:
     Returns the value of the removed Node."""
 
     def remove_from_head(self):
-        self.length -= 1
-        # if no current head return False
-        # if current head exists
-        # set the current head to a temp
-        #
-        pass
+        # Get the value of the head node we want to delete
+        value = self.head.value
+        # Call the delete method on the head node
+        self.delete(self.head)
+        # And return the deleted value
+        return value
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
 
     def add_to_tail(self, value):
+        # increment the length of the list on addition
         self.length += 1
-
+        # instantiate a new node setting prev and next to None
         new_node = ListNode(value, None, None)
-
+        # Check if head and tail already exists (there is at least 1 item in the list)
+        # if no head and tail exists already
         if not self.tail and not self.head:
             self.tail = new_node
             self.head = new_node
@@ -114,7 +117,12 @@ class DoublyLinkedList:
     Returns the value of the removed Node."""
 
     def remove_from_tail(self):
-        pass
+        # Get the value of the tail node we want to delete
+        value = self.tail.value
+        # Call the delete method on the tail node
+        self.delete(self.tail)
+        # And return the deleted value
+        return value
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
@@ -132,7 +140,34 @@ class DoublyLinkedList:
     the node was the head or the tail"""
 
     def delete(self, node):
-        pass
+        # decrement the length of the list on deletion
+        self.length -= 1
+        # Check if head and tail already exists (there is at least 1 item in the list)
+        # if no head and tail exists already
+        if not self.head and not self.tail:
+            # return None
+            return
+        # otherwise if the current head is the same as the current tail (we have only 1 item in the list)
+        elif self.head == self.tail:
+            # set the both the head and tail to None
+            self.head = None
+            self.tail = None
+        # Else If the current head is the same as the node taken in this delete method
+        elif self.head == node:
+            # set the current head to the item next to the current head
+            self.head = self.head.next
+            # call the ListNode delete method on the node
+            node.delete()
+        # Else If the current tail is the same as the node taken in this delete method
+        elif self.tail == node:
+            # set the current tail to the item next to the current tail
+            self.tail = self.tail.next  # or is it self.tail.prev?
+            # call the ListNode delete method on the node
+            node.delete()
+        # If the node to be deleted is in-between
+        else:
+            # call delete on the node
+            node.delete()
 
     """Returns the highest value currently in the list"""
 
